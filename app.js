465b508eb15c9e2c6044639d03464bdb2a5f4e9e
Razi0608 <razirangrez2003@gmail.com>
2023-12-06 19:34:53 +0530

    modified

--- app.js
+++ app.js
@@ -6,8 +6,6 @@ const routes = require("./routes/router");
 const mongoose = require("mongoose")
  require("dotenv").config()
 
-
-
 mongoose.connect(process.env.MONGO_DB_URL)
 .then(() => console.log("Database Connected"))
 .catch((err) => {
